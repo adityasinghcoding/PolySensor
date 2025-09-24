c23151a137e97eadb992b029bf4f0ff5493430ca
Aditya Singh <adityabussinesslimited@gmail.com>
2025-09-24 11:22:09 +0530

    updated the main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from prompts import JSON_DOCUMENT, AUDIO_TEXT, VIDEO_TEXT, IMAGE_TEXT
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
+from text_extractor import (final_audio_prompt, final_doc_prompt, final_image_prompt, final_video_prompt,
+                            unstructured_doc_json_data, audio_data, video_audio_data, image_data)
 
 load_dotenv() # loading the environment variables from .env
 
@@ -27,14 +29,18 @@ video_chain = LLMChain(llm=llm, prompt = video_prompt)
 
 # running feeding the output of functions with prompts to llm to get output
 if unstructured_doc_json_data is not None:
-   llm_doc_output = document_chain.run(doc_json_data = unstructured_doc_json_data)
+   llm_doc_output = document_chain.run(doc_json_data = final_doc_prompt)
    print(llm_doc_output)
 
 if audio_data is not None:
-   llm_audio_output = audio_chain.run(audio_data = audio_data)
+   llm_audio_output = audio_chain.run(audio_data = final_audio_prompt)
    print(llm_audio_output)
 
-if video_data is not None:
-   llm_video_output = video_chain.run(video_audio_data= video_audio_data)
-   print(llm_video_output)
+if image_data is not None:
+   llm_image_output = video_chain.run(image_data= final_image_prompt)
+   print(llm_image_output)
+
+if video_audio_data is not None:
+   llm_video_audio_output = video_chain.run(video_audio_data= final_video_prompt)
+   print(llm_video_audio_output)
 
