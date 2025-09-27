4e7fedf7cf23200dd4240d97df614ac36f56e75a
Aditya Singh <adityabussinesslimited@gmail.com>
2025-09-27 18:05:10 +0530

    some minor finetuning

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import AnalyzePage from './pages/AnalyzePage';
 import './App.css';
+import logo from '../../assets/PolySensor no bg 200px.png';
 
 function App() {
    return (
       <div className='app'>
          <header className='app-header'>
+            <img src={logo} className="app-logo" alt="Custom Logo" /> {/* Add the img tag here */}
             <h1>PolySensor</h1>
-            <p>Upload documents, images, audio, or video for AI analysis</p>
+            <p>AI-Powered Multi-Modal Content Analyzer</p>
          </header>
 
          <main className='app-main'>
